11e420da4b0c4506e5f4fbe47c55518ff3feb4e2
john <nergilz@yandex.ru>
2018-04-04 01:48:13 +0200

    first version the script

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
-# Anti-Duplicator
+## Anti-Duplicator
 
-[TODO. There will be project description]
+This script find the duplicates files
 
-# Project Goals
+
+### Discription on the script
+
++ specify the path to start
++ the script will print duplicates files with path and size
+
+
+### Script start example
+```bash
+python3 duplicates.py /home/comp/devman
+```
+
+### Example result
+```bash
+ Path: /home/comp/devman/8_vk_friends_online/8vkenv/lib/python3.5/os.py  Size: 36970
+ Path: /home/comp/devman/formatting_json/venv/lib/python3.5/os.py  Size: 36970
+ Path: /home/comp/devman/4_json/.git/hooks/commit-msg.sample  Size: 896
+ Path: /home/comp/devman/3_bars/.git/hooks/commit-msg.sample  Size: 896
+```
+
+### Requirements
+```bash
+Python ver 3.5 (or higher)
+```
+
+---
+## Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,62 @@
+import os
+import argparse
+
+
+def get_files_free(path):
+    files_tree = []
+    files_size_and_path = {}
+
+    for dir_path, dir_names, file_names in os.walk(path):
+        for file_name in file_names:
+            path_file = os.path.join(dir_path, file_name)
+            size = os.path.getsize(path_file)
+            path_file = list(os.path.split(path_file))
+            path_file.append(str(size))
+            files_tree.append(path_file)
+    for path, name, size in files_tree:
+        name_and_size = name + ' ' + size
+        files_size_and_path.setdefault(name_and_size, []).append(path)
+
+    return files_size_and_path
+
+
+def get_duplicates(files_size_and_path):
+    duplicates_files = []
+    for file, paths in files_size_and_path.items():
+        if len(paths) > 1:
+            for path in paths:
+                res = path + '/' + file
+                duplicates_files.append(res)
+    return duplicates_files
+
+
+def pprint_duplicates(duplicates_files):
+    for file_inform in duplicates_files:
+        information = file_inform.split()
+        path = information[0]
+        size = information[1]
+        print(' Path: {0}  Size: {1}'.format(path, size))
+
+
+def get_parser_args():
+    parser = argparse.ArgumentParser(
+        description='Path to check for duplication'
+    )
+    parser.add_argument(
+        'path',
+        help='input path to check'
+    )
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-    pass
+    try:
+        arguments = get_parser_args()
+        if os.path.isdir(arguments.path):
+            files_tree = get_files_free(arguments.path)
+            duplicates_files = get_duplicates(files_tree)
+            pprint_duplicates(duplicates_files)
+        else:
+            print(' ERROR: this is a file, you need a directory')
+    except FileNotFoundError as ex:
+        print(' ERROR: {}'.format(ex))

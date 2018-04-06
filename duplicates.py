d204e35c49c4c38b089128e67378277cde2d301c
john <nergilz@yandex.ru>
2018-04-07 01:08:27 +0200

    many veriables deleted 'example[number]'

--- duplicates.py
+++ duplicates.py
@@ -2,37 +2,39 @@ import os
 import argparse
 
 
-def get_files_size_and_path(path):
-    files_size_and_path = {}
+def get_size_path_files(path):
+    size_and_path_for_files = {}
 
-    for dir_path, dir_names, file_names in os.walk(path):
-        for file_name in file_names:
-            path_file = os.path.join(dir_path, file_name)
+    for dir_path, dir_names, names_files in os.walk(path):
+        for name_file in names_files:
+            path_file = os.path.join(dir_path, name_file)
             size_file = os.path.getsize(path_file)
-            path_file = os.path.split(path_file)
-            inform_file = (path_file[0], (path_file[1], str(size_file)))
-            files_size_and_path.setdefault(inform_file[1], []).append(inform_file[0])
+            size_and_path_for_files.setdefault(
+                (name_file, size_file),
+                []
+            ).append(path_file)
 
-    return files_size_and_path
-
-
-def get_duplicates(files_size_and_path):
-    duplicates_files = {}
-
-    for name_size_file, paths_file in files_size_and_path.items():
-        if len(paths_file) > 1:
-            for path in paths_file:
-                duplicates_files.setdefault(name_size_file, []).append(path)
-    return duplicates_files
+    return size_and_path_for_files
 
 
-def pprint_duplicates(duplicates_files):
+def get_duplicates(size_and_path_for_files):
+    duplicate_files = {}
 
-    for name_size_file, paths_file in duplicates_files.items():
-        for path in paths_file:
-            name_file = name_size_file[0]
-            size_file = name_size_file[1]
-            path_file = os.path.join(path, name_file)
+    for (name_file, size_file), paths_files in size_and_path_for_files.items():
+        if len(paths_files) > 1:
+            for path_file in paths_files:
+                duplicate_files.setdefault(
+                    (name_file, size_file),
+                    []
+                ).append(path_file)
+
+    return duplicate_files
+
+
+def pprint_duplicate(duplicate_files):
+
+    for (name_file, size_file), paths_files in duplicate_files.items():
+        for path_file in paths_files:
             print(' PATH: {0}  SIZE: {1}'.format(path_file, size_file))
 
 
@@ -48,11 +50,11 @@ def get_parser_args():
 
 
 if __name__ == '__main__':
-
     arguments = get_parser_args()
+
     if os.path.isdir(arguments.path):
-        files_size_and_path = get_files_size_and_path(arguments.path)
-        duplicates_files = get_duplicates(files_size_and_path)
-        pprint_duplicates(duplicates_files)
+        size_and_path_for_files = get_size_path_files(arguments.path)
+        duplicate_files = get_duplicates(size_and_path_for_files)
+        pprint_duplicate(duplicate_files)
     else:
         print(' ERROR: this is a file, or directory not found')
